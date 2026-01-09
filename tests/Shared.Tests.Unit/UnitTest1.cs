3f42e9e662905e095c61f46662d09171b81e0ad7
ArticleManagementApp Dev <dev@articlemgmt.local>
2026-01-09 11:14:46 -0800

    feat: add xUnit test project scaffolding

---
+++ tests/Shared.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Tests.Unit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/Web.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.Tests.Integration;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
